70659ef7dd597a08c5b6503820af17186f9d65aa
praveen t n <tnpraveen87@gmail.com>
2018-04-23 13:02:43 +0530

    Update bulkMatchAndReplace.js

--- bulkMatchAndReplace.js
+++ bulkMatchAndReplace.js
@@ -7,12 +7,12 @@ gulp.task('default', function() {
     // values in users1 with corresponding values in users2
     // writes to corresponding build/*.xml files
     
-    var users1 = ["existing_value1", "existing_value2", "existing_value3"];
-    var users2 = ["new_value1", "new_value2", "new_value3"];
+    var array1 = ["existing_value1", "existing_value2", "existing_value3"];
+    var array2 = ["new_value1", "new_value2", "new_value3"];
     var stream = gulp.src(['input/*.xml']); 
     
     for (i=0;i<users1.length;i++) { 
-        stream = stream.pipe(replace(users1[i], users2[i])); 
+        stream = stream.pipe(replace(array1[i], array2[i])); 
         } 
     return stream.pipe(gulp.dest('build/')
     ); 
